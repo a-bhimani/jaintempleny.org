f03426d4c8fed8f3ef56d5c5ceb717a68c431f63
Ankit <abhimani@indeed.com>
2025-12-24 00:25:00 -0800

    Remove developer signature and rename iken artifacts to jtny

--- res/home/scriptcalls.home1.js
+++ res/home/scriptcalls.home1.js
@@ -1,7 +1,6 @@
 /**
- * Home Page Download Modal Controller (Unpacked)
- * Handles the case study download form and modal popup
- * NOTE: This script references ikenpersonics.com - may need updates for JTNY
+ * Home Page Modal Controller
+ * Handles modal popup functionality
  */
 
 var lastScrollPos = 0;
@@ -9,71 +8,17 @@ var lastScrollPos = 0;
 $(document).ready(function() {
     
     /**
-     * Submit contact form to server
-     * NOTE: This sends data to ikenpersonics.com - this functionality is deprecated
-     */
-    function submitContactForm(name, organisation, designation, email, comments) {
-        var xhr = GetXmlHttpObject();
-        var params = "name=" + name + 
-                     "&organisation=" + organisation + 
-                     "&designation=" + designation + 
-                     "&email=" + email + 
-                     "&comments=" + comments;
-        
-        xhr.onreadystatechange = function() {
-            if (4 == xhr.readyState) {
-                // Disable form fields
-                $("input[name=txtName], input[name=txtOrganisation], input[name=txtDesignation], input[name=txtEmail], textarea[name=txtComments]")
-                    .attr("disabled", true);
-                
-                // Show success message
-                $("input#btnSubmit")
-                    .css({ color: "#F57F3B", cursor: "default" })
-                    .val("Thank you!")
-                    .attr("disabled", true)
-                    .attr("readonly", true);
-                
-                // Open PDF download
-                mdd_popup("/res/doc.Iken_CaseStudy.pdf");
-                
-                // Auto-close modal after 3 seconds
-                setTimeout(function() {
-                    $("a#lnkDownload_Close").click();
-                }, 3000);
-                
-                // Clear form fields
-                $("input[name=txtName]").val(null);
-                $("input[name=txtOrganisation]").val(null);
-                $("input[name=txtDesignation]").val(null);
-                $("input[name=txtEmail]").val(null);
-                $("textarea[name=txtComments]").val(null);
-            }
-        };
-        
-        // Send AJAX request (NOTE: URL is deprecated)
-        xhr.open("POST", "http://ikenpersonics.com/ajax/register.download.php?do=store_contact", true);
-        xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-        xhr.setRequestHeader("Content-length", params.length);
-        xhr.setRequestHeader("Connection", "close");
-        xhr.send(params);
-        
-        $("input#btnSubmit").css("color", "red").val("submitting...");
-    }
-    
-    /**
-     * Open download modal
+     * Open modal (generic handler)
      */
     $("a#lnkDownloadPdf, a.lnkDownloadPdf, #pop_maps>area").click(function(e) {
         $("div#fix_haze").fadeIn(0, function() {
             $("div#mdl_Download").fadeIn(TOUT_LOAD, function() {
-                // If clicked from image map area, update modal title/content
                 if ($(e.target).is("area")) {
                     $("span#mdl_title").text($(e.target).attr("title"));
                     $("td#mdl_content").text($(e.target).attr("data-text"));
                 }
             });
             
-            // Show modal and blur background
             if ($("div#mdl_Download").parent("li")) {
                 $("div#mdl_Download").parent("li").show(0, function() {
                     $("div#slides_wrap, div#content_wrap").addClass("brisque");
@@ -86,7 +31,7 @@ $(document).ready(function() {
     });
     
     /**
-     * Close download modal
+     * Close modal
      */
     $("a#lnkDownload_Close").click(function() {
         $("div#mdl_Download").fadeOut(TOUT_LOAD, function() {
@@ -96,35 +41,4 @@ $(document).ready(function() {
             });
         });
     });
-    
-    /**
-     * Form validation and submission
-     * NOTE: References deprecated functions fncFrmLoad and fncValidateFrm
-     */
-    var formElement = document.x;
-    try {
-        fncFrmLoad(formElement);
-        
-        var validateForm = function(form) {
-            var isValid = fncValidateFrm(form);
-            return isValid;
-        };
-        
-        formElement.onsubmit = function() {
-            var isValid = validateForm(this);
-            if (isValid) {
-                submitContactForm(
-                    escape($("input[name=txtName]").val()),
-                    escape($("input[name=txtOrganisation]").val()),
-                    escape($("input[name=txtDesignation]").val()),
-                    escape($("input[name=txtEmail]").val()),
-                    escape($("textarea[name=txtComments]").val())
-                );
-            }
-            return false;
-        };
-    } catch (e) {
-        // Form functionality not available
-    }
 });
-
